8c6b3fc485e4970f8343b47bcefe32a45b18efa1
SaravanaAsk123 <saravanamechdme@gmail.com>
2022-01-20 12:01:37 +0530

    passwort functionality

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,5 +1,15 @@
 package org.test;
 
-public class Sample {
+import org.openqa.selenium.UsernameAndPassword;
+import org.openqa.selenium.grid.Main;
 
+public class Sample {
+private void username() {
+System.out.println("saravana");
+
+}
+public static void main(String[] args) {
+	Sample a= new Sample();
+	a.username();
+}
 }
